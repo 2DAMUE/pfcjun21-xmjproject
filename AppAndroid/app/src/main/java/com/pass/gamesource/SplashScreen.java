125dab7d68b590e5a7d8e04565e0c07faa8e6567
Xavier Robles <Xavier.robles.r@gmail.com>
2021-04-17 20:33:50 +0200

    Javadoc, ajuste de tiempo

--- AppAndroid/app/src/main/java/com/pass/gamesource/SplashScreen.java
+++ AppAndroid/app/src/main/java/com/pass/gamesource/SplashScreen.java
@@ -11,34 +11,41 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
+/**
+ * @author XMJproject
+ * @author Xavier R, Jorge B, Miguel Parra
+ * @version 0.01
+ * @see HomeActivity
+ * @see anim
+ * @see layout\activity_splash_screen.xml
+ */
 public class SplashScreen extends AppCompatActivity {
     private MediaPlayer mediaplayer;
-    private ImageView iv_fondo_logoG;
-    private ImageView iv_fondo_logoS;
-    private ImageView iv_fondo_G;
-    private ImageView iv_fondo_S;
-    private ImageView iv_encadenadas;
-    private ImageView iv_game;
-    private ImageView iv_sorce;
-    private ImageView iv_gameSourcer;
+    private ImageView iv_fondo_logoG, iv_fondo_logoS, iv_fondo_G, iv_fondo_S, iv_encadenadas, iv_game, iv_source, iv_gameSourcer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        initialization();
+    }
+
+    public void initialization() {
+        /**
+         * Statement of the sound.
+         */
         mediaplayer = MediaPlayer.create(this, R.raw.mystery);
-
-
-        //implements and starts animations
-        iv_fondo_logoG= findViewById(R.id.iv_fondo_logoG);
-        iv_fondo_logoS= findViewById(R.id.iv_fondo_logoS);
-        iv_fondo_G= findViewById(R.id.iv_G);
-        iv_fondo_S= findViewById(R.id.iv_S);
+        /**
+         * implements and starts animations
+         */
+        iv_fondo_logoG = findViewById(R.id.iv_fondo_logoG);
+        iv_fondo_logoS = findViewById(R.id.iv_fondo_logoS);
+        iv_fondo_G = findViewById(R.id.iv_G);
+        iv_fondo_S = findViewById(R.id.iv_S);
         iv_encadenadas = findViewById(R.id.iv_encadenadas);
         iv_game = findViewById(R.id.iv_game);
-        iv_sorce = findViewById(R.id.iv_source);
+        iv_source = findViewById(R.id.iv_source);
         iv_gameSourcer = findViewById(R.id.iv_gameSource);
-
 
         Animation animLeftlogoG = AnimationUtils.loadAnimation(this, R.anim.moveleftt);
         Animation animRightlogoS = AnimationUtils.loadAnimation(this, R.anim.moveright);
@@ -49,39 +56,32 @@ public class SplashScreen extends AppCompatActivity {
         Animation animReboteSource = AnimationUtils.loadAnimation(this, R.anim.rebotesource);
         Animation animGameSourceAparece = AnimationUtils.loadAnimation(this, R.anim.aphagamesourcer);
 
-
         iv_fondo_logoG.startAnimation(animLeftlogoG);
         iv_fondo_logoS.startAnimation(animRightlogoS);
         iv_fondo_G.startAnimation(animUplogoG);
         iv_fondo_S.startAnimation(animDownlogoS);
         iv_encadenadas.startAnimation(animAparecerEncadenadas);
         iv_game.startAnimation(animReboteGame);
-        iv_sorce.startAnimation(animReboteSource);
+        iv_source.startAnimation(animReboteSource);
         iv_gameSourcer.startAnimation(animGameSourceAparece);
-
-
-       //
-        openApp(true);
-        //openApp2(true);
+        openApp();
         mediaplayer.start();
     }
 
-
-    private void openApp(boolean locationPermission) {
-
+    /**
+     * Change of activity
+     * Time in milliseconds
+     */
+    private void openApp() {
         new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
             @Override
             public void run() {
-
                 Intent intent = new Intent(SplashScreen
-                        .this, MainActivity.class); //Cambiar aqui para que pase a la nueva pantalla
+                        .this, MainActivity.class);
                 startActivity(intent);
                 finish();
-
             }
-        }, 4000);
-
+        }, 5000);
     }
-
 
 }
