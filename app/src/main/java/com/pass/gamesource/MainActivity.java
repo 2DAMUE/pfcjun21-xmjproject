37d511aafc0e4a9cebf06ff17ee787257e8d874d
Jorge Ballesteros <jorgitoballes@gmail.com>
2021-05-09 21:41:17 +0200

    Ajustes generales en el Main y cambio de colores y tamaños en el alertdialogmain.xml

--- app/src/main/java/com/pass/gamesource/MainActivity.java
+++ app/src/main/java/com/pass/gamesource/MainActivity.java
@@ -34,32 +34,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         //Creamos el ArrayList con todos los videojuegos para que se muestren en el Recycler
         ArrayList<Videojuego> listaVideojuegos = new ArrayList<Videojuego>();
-        Videojuego v = new Videojuego("No man's Sky", "10.90", "Exploración y aventura a saco paco", "https://store-images.s-microsoft.com/image/apps.15909.68818099466568894.ac2f77eb-933c-43dd-9097-146a94c389a9.2490b939-a2cb-41b7-8816-8792d37a5338");
-        Videojuego v2 = new Videojuego("Albion Online", "0", "MMORPG", "https://videoguejos.com/wp-content/uploads/2018/05/Albion-Online.png");
-        Videojuego v3 = new Videojuego("Call of duty", "0", "FPS", "https://image.api.playstation.com/vulcan/img/cfn/11307CjjUZ9rA_whmJUghJsG9Hl1-rmnOUTk3-nccj01ZpYMCHrJ8k8kzBrVyp-p-iCPej73TEJAs88ZBeiZ1uirtj0fsa16.png");
-        Videojuego v4 = new Videojuego("Fortnite", "0", "Battle royale de acción", "https://cdn2.unrealengine.com/16br-agentjonesy-egs-s2-1200x1600-1200x1600-2531e05bb04f.jpg");
-        Videojuego v5 = new Videojuego("Destiny 2", "0", "FPS de rol ambientado en el espacio", "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2017/06/destiny_2_caratula.png?itok=3014kE94");
-        Videojuego v6 = new Videojuego("Ark", "0", "Juego de rol ambientado en la prehistoria", "https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Fark%2Fhome%2FEGS_ARKSurvivalEvolved_StudioWildcard_S2-1200x1600-880bbe4ea103bc00a67c12144161bdcfb1c73df1.jpg");
-        Videojuego v7 = new Videojuego("cities Skylines", "0", "Juego de contrucción de ciudades", "https://cdn2.unrealengine.com/egs-citiesskylines-colossalorder-s5-1920x1080-689706625.jpg?h=1080&resize=1&w=1920");
-        Videojuego v8 = new Videojuego("Asseto Corsa", "0", "Juego de simulación de conducción", "https://image.api.playstation.com/cdn/EP4040/CUSA01797_00/NMcAucyANMnYMNkz6V5vk9f5YXty2mCz.png");
-        Videojuego v9 = new Videojuego("Euro Truck Simulator", "0", "Juego de simulación de conducción de camiones", "https://s1.gaming-cdn.com/images/products/309/orig/euro-truck-simulator-2-cover.jpg");
 
-        listaVideojuegos.add(v);
-        listaVideojuegos.add(v2);
-        listaVideojuegos.add(v3);
-        listaVideojuegos.add(v4);
-        listaVideojuegos.add(v5);
-        listaVideojuegos.add(v6);
-        listaVideojuegos.add(v7);
-        listaVideojuegos.add(v8);
-        listaVideojuegos.add(v9);
+        listaVideojuegos.add(new Videojuego("No man's Sky", "10.90", "Exploración y aventura a saco paco", "https://store-images.s-microsoft.com/image/apps.15909.68818099466568894.ac2f77eb-933c-43dd-9097-146a94c389a9.2490b939-a2cb-41b7-8816-8792d37a5338"));
+        listaVideojuegos.add(new Videojuego("Albion Online", "0", "MMORPG", "https://videoguejos.com/wp-content/uploads/2018/05/Albion-Online.png"));
+        listaVideojuegos.add(new Videojuego("Call of duty", "0", "FPS", "https://image.api.playstation.com/vulcan/img/cfn/11307CjjUZ9rA_whmJUghJsG9Hl1-rmnOUTk3-nccj01ZpYMCHrJ8k8kzBrVyp-p-iCPej73TEJAs88ZBeiZ1uirtj0fsa16.png"));
+        listaVideojuegos.add(new Videojuego("Fortnite", "0", "Battle royale de acción", "https://cdn2.unrealengine.com/16br-agentjonesy-egs-s2-1200x1600-1200x1600-2531e05bb04f.jpg"));
+        listaVideojuegos.add(new Videojuego("Destiny 2", "0", "FPS de rol ambientado en el espacio", "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2017/06/destiny_2_caratula.png?itok=3014kE94"));
+        listaVideojuegos.add(new Videojuego("Ark", "0", "Juego de rol ambientado en la prehistoria", "https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Fark%2Fhome%2FEGS_ARKSurvivalEvolved_StudioWildcard_S2-1200x1600-880bbe4ea103bc00a67c12144161bdcfb1c73df1.jpg"));
+        listaVideojuegos.add(new Videojuego("cities Skylines", "0", "Juego de contrucción de ciudades", "https://cdn2.unrealengine.com/egs-citiesskylines-colossalorder-s5-1920x1080-689706625.jpg?h=1080&resize=1&w=1920"));
+        listaVideojuegos.add(new Videojuego("Asseto Corsa", "0", "Juego de simulación de conducción", "https://image.api.playstation.com/cdn/EP4040/CUSA01797_00/NMcAucyANMnYMNkz6V5vk9f5YXty2mCz.png"));
+        listaVideojuegos.add(new Videojuego("Euro Truck Simulator", "0", "Juego de simulación de conducción de camiones", "https://s1.gaming-cdn.com/images/products/309/orig/euro-truck-simulator-2-cover.jpg"));
 
         ImageView ivMain = findViewById(R.id.JuegoPrincipal);
         Glide.with(this)
                 .load("https://firebasestorage.googleapis.com/v0/b/gamesource-9bc51.appspot.com/o/epic_free%2FAlien%3A%20Isolation%20.JPEG?alt=media")
                 .centerCrop().into(ivMain);
 
-        Log.d("", v.toString());
         RecyclerView recyclerView = findViewById(R.id.recyclerMain);
         RecyclerView.LayoutManager gestor = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.HORIZONTAL, false);
         AdaptadorRecyclerMain adaptador = new AdaptadorRecyclerMain(listaVideojuegos, this);
@@ -115,6 +105,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Intent intent11 = new Intent(MainActivity.this, CalendarActivity.class);
                 startActivity(intent11);
                 break;
+            case R.id.JuegoPrincipal:
+
 
         }
 
